6f5a6d906b6dc036bfcbfd10b855b3d9bf65a1e0
HuJun017 <166590848+HuJun017@users.noreply.github.com>
2025-11-05 07:24:04 +0000

    catalogo dei prodotti

--- MediaBuy/src/app/components/pagina-2-catalogo/pagina-2-catalogo.ts
+++ MediaBuy/src/app/components/pagina-2-catalogo/pagina-2-catalogo.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Prodotto } from '../prodotto/prodotto';
+import { ProdottiService } from '../../services/prodotti/prodotti';
 
 @Component({
   selector: 'app-pagina-2-catalogo',
-  imports: [],
+  imports: [CommonModule, Prodotto],
   templateUrl: './pagina-2-catalogo.html',
   styleUrl: './pagina-2-catalogo.css',
 })
 export class Pagina2Catalogo {
+  prodotti: any[] =  [];
 
+  constructor(private ProdottiService: ProdottiService) {}
+
+  ngOnInit() {
+    this.prodotti = this.ProdottiService.getProdotti();
+  }
+
+  mostraProdotti = true;
+  event_mostraProdotti () {
+    this.mostraProdotti = !this.mostraProdotti; 
+  }
 }
