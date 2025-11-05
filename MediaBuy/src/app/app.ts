40296a8d689efcd43d93e47013f0fa9ddc056352
HuJun017 <166590848+HuJun017@users.noreply.github.com>
2025-11-05 07:25:34 +0000

    fixes

--- MediaBuy/src/app/app.ts
+++ MediaBuy/src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Pagina1Offerte } from './components/pagina-1-offerte/pagina-1-offerte';
+import { Pagina2Catalogo } from './components/pagina-2-catalogo/pagina-2-catalogo';
+import { Pagina3Termini } from './components/pagina-3-termini/pagina-3-termini';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterOutlet, Pagina1Offerte, Pagina2Catalogo, Pagina3Termini, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('MediaBuy');
+
 }
