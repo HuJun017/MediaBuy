2d9a0a9a5373cee09982bce1ea51f2a056c61660
HuJun017 <166590848+HuJun017@users.noreply.github.com>
2025-11-05 07:25:02 +0000

    endpoints

--- MediaBuy/src/app/app.routes.ts
+++ MediaBuy/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Pagina1Offerte } from './components/pagina-1-offerte/pagina-1-offerte';
+import { Pagina2Catalogo } from './components/pagina-2-catalogo/pagina-2-catalogo';
+import { Pagina3Termini } from './components/pagina-3-termini/pagina-3-termini';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'pagina1', pathMatch: 'full' },
+  { path: 'pagina1', component: Pagina1Offerte },
+  { path: 'pagina2', component: Pagina2Catalogo },
+  { path: 'pagina3', component: Pagina3Termini },
+  { path: '**', redirectTo: '/pagina1' } 
+];
